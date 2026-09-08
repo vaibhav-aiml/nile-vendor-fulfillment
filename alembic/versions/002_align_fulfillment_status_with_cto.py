0954fbb4934c9cd7644989796791e640bdff415b
Vaibhav Badaya <vaibhavbadaya53@gmail.com>
2026-09-08 19:13:38 +0530

    fix(migration): set correct down_revision to 001_initial_schema and add backup script

--- alembic/versions/002_align_fulfillment_status_with_cto.py
+++ alembic/versions/002_align_fulfillment_status_with_cto.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '002'
-down_revision = '001'
+down_revision = '001_initial_schema'
 branch_labels = None
 depends_on = None
 
